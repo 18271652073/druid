66f1d4274c5195808b1dff48e80a6fe4a075066c
dukaixiang <827739960@qq.com>
2019-03-08 09:55:34 +0800

    03-08  提交1212

---
+++ src/main/java/com/nbdeli/demo/util/Test.java
@@ -0,0 +1,28 @@
+package com.nbdeli.demo.util;
+
+import java.text.ParseException;
+
+/**
+ * @author Administrator
+ * @date 2019/3/8.
+ */
+public class Test {
+    public static void main(String[] args) throws ParseException {
+//        Date date = DateUtils.date2Sub(DateUtils.str2Date("2016-03-02 20:16:21", "yyyy-MM-dd HH:mm:ss"), 12, 10);
+//        System.out.println(DateUtils.date2Str(new Date(), "yyyyMMddHHmmss"));
+        // Date date1 = new Date();
+        // System.out.println(date1);
+        // System.out.println(date1.before(date));
+        // String start = "2016-06-20 16:42:41";
+        // String end = "2017-06-21 00:00:00";
+        // SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
+        // Date startTime = sf.parse(start);
+        // Date endTime = sf.parse(end);
+        // long days = dayDiff(startTime, endTime);
+        // System.out.println(days);
+        // String s = DateUtils.str2DateFormat("1468466182000", "yyyy-MM-dd");
+        // System.out.println(s);
+//        System.out.println(getDateAddOne("2018-02-28"));
+//        System.out.println(getMonthFirstDay(2018,1));
+    }
+}
